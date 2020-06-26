74a77746dd4e9f266124c12a272c778002362987
Dan Church <dchurch@g2planet.com>
2020-06-26 17:32:45 -0500

    Configure project to my liking
    - Configure editor.
    - Add bootstrap.
    - Configure linting, TypeScript strictness.

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,17 @@
-import { browser, by, element } from 'protractor';
+import {
+    browser,
+    by,
+    element,
+} from 'protractor';
 
 export class AppPage {
-  navigateTo(): Promise<unknown> {
-    return browser.get(browser.baseUrl) as Promise<unknown>;
-  }
 
-  getTitleText(): Promise<string> {
-    return element(by.css('app-root .content span')).getText() as Promise<string>;
-  }
+    public async navigateTo(): Promise<unknown> {
+        return browser.get(browser.baseUrl) as Promise<unknown>;
+    } // end navigateTo()
+
+    public async getTitleText(): Promise<string> {
+        return element(by.css('app-root .content span')).getText() as Promise<string>;
+    } // end getTitleText()
+
 }
